e8ff4d9ee6e91bf6160cf70f5f114cb1efa00aac
NhloniNkwinika <hloninkwinika@gmail.com>
2025-01-07 14:26:34 +0200

    server,js

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,21 @@
 import express from 'express';
 import dotenv from "dotenv";
-
+//import Product from './modules/products.model.js';
 import { connectDB } from './config/db.js';
-
+//import mongoose from 'mongoose';
+import productRoutes from "./routes/product.route.js";
 dotenv.config();
 
 const app = express();
 app.use(express.json()); // allows to use json in the body
 
-app.post("/api/products", (req,res) => {
-   const product = req.body; //user will send this data
-
-   if (!product.name || !p.approduct.price || !product.image){
-    return res.status(400).json({success:false, message: "Please provide all the fields"});
-   }
-   const newProduct = new Product(product);
-
-   try{
-    newProduct.save();
-    res.status(201).json({success:true, data:newProduct});
-   }
-    catch(error){
-    console.error("error in create product:", error.message);
-    res.status(500).json({sucess:false, message:"server error"});
-    }
-   
-
-});
-
-//console.log(process.env.MONGO_URI)
-
+app.use("/api/products", productRoutes)
 
 app.listen(5000, () => {
     connectDB();
     console.log("Server started at http://localhost:5000 ");
 });
+
 
 //jFxxwimn5FeB6xcx
 //npm install mongodb
